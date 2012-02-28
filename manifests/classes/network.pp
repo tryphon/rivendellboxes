0a1751a2ba25036d806e4bb2a2885a6a2a388adf
Alban Peignier <alban@tryphon.eu>
2012-02-28 22:22:42 +0100

    Install dhcp3-client before sethostname hook

--- manifests/classes/network.pp
+++ manifests/classes/network.pp
@@ -3,6 +3,7 @@ class network::dhclient::hostname {
   
   file { "/etc/dhcp/dhclient-enter-hooks.d/sethostname":
     source => "puppet:///files/network/sethostname",
-    mode => 755
+    mode => 755,
+    require => Package[dhcp3-client]
   }
 }
