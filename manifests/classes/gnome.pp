67e0442374c9e45138880fa9e77b17ad46c57776
Alban Peignier <alban@tryphon.eu>
2012-04-16 08:03:22 +0200

    Add gnome-screenshot. Fixes #3

--- manifests/classes/gnome.pp
+++ manifests/classes/gnome.pp
@@ -22,13 +22,11 @@ class gnome::minimal {
   
   package { xserver-xorg: }
   # Selected from gnome-core dependencies
-  package { [ desktop-base,dmz-cursor-theme,eog,epiphany-browser,evince,file-roller,gedit,gnome-control-center,gnome-icon-theme,gnome-keyring,gnome-menus,gnome-panel,gnome-power-manager,gnome-session,gnome-settings-daemon,gnome-terminal,gtk2-engines,gvfs,gvfs-backends,metacity,nautilus,policykit-1-gnome,gnome-themes ]: }
+  package { [ desktop-base,dmz-cursor-theme,eog,epiphany-browser,evince,file-roller,gedit,gnome-control-center,gnome-icon-theme,gnome-keyring,gnome-menus,gnome-panel,gnome-power-manager,gnome-session,gnome-settings-daemon,gnome-terminal,gtk2-engines,gvfs,gvfs-backends,metacity,nautilus,policykit-1-gnome,gnome-themes,gnome-screenshot ]: }
 }
 
 class gnome::vnc {
-
   package { vino: }
-
 }
 
 
