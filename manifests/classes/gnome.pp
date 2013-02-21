ac8aa0d1ce2262fcee1cacbc5dcd6d23e11b98cf
Alban Peignier <alban@tryphon.eu>
2013-02-21 14:39:54 +0100

    Add x11-xserver-utils package to provide xset. Refs #435

--- manifests/classes/gnome.pp
+++ manifests/classes/gnome.pp
@@ -25,6 +25,7 @@ class gnome::minimal {
   package { [ desktop-base,dmz-cursor-theme,eog,epiphany-browser,evince,file-roller,gedit,gnome-control-center,gnome-icon-theme,gnome-keyring,gnome-menus,gnome-panel,gnome-power-manager,gnome-session,gnome-settings-daemon,gnome-terminal,gtk2-engines,gvfs,gvfs-backends,metacity,nautilus,policykit-1-gnome,gnome-themes,gnome-screenshot ]: }
 
   include gnome::polkit
+  package { "x11-xserver-utils": }
 }
 
 class gnome::vnc {
