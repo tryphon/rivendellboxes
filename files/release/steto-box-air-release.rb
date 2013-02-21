530336677619c0da0fbfca0e9ea101bce0dcf3b1
Alban Peignier <alban@tryphon.eu>
2013-02-21 14:37:12 +0100

    Fixes release name checked in steto box-air-release. Fixes #672

--- files/release/steto-box-air-release.rb
+++ files/release/steto-box-air-release.rb
@@ -1,6 +1,6 @@
 Steto.config do
   check :box_air_release do
-    box_air_release = box.release.gsub("all","air")
+    box_air_release = box.release.gsub("nas","air")
     File.exists? "/srv/rivendell/boot/initrd-#{box_air_release}.img"
   end
 end
