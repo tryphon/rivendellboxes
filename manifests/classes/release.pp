755ce5f94097cdef45feed63e1d61a5b217f070a
Alban Peignier <alban@tryphon.eu>
2014-03-24 10:34:22 +0100

    Use boxes-upgrade as Box::Release.install_command. Refs #1026

--- manifests/classes/release.pp
+++ manifests/classes/release.pp
@@ -10,16 +10,20 @@ class release::airbox {
     require => Package[cron]
   }
 
-  steto::conf { box-air-release: 
+  steto::conf { box-air-release:
     source => "puppet:///files/release/steto-box-air-release.rb"
   }
 }
 
 class release::boxes {
-
   file { "/usr/local/sbin/boxes-upgrade":
     source => "puppet:///files/release/boxes-upgrade",
     mode => 755
   }
-
+  file { "/etc/box/local.d/box-release-with-boxes-upgrade":
+    content => "Box::Release.install_command = '/usr/local/sbin/boxes-upgrade'\n"
+  }
+  sudo::line { "www-data-boxes-upgrade":
+    line => "www-data	ALL=(root) NOPASSWD: /usr/local/sbin/boxes-upgrade"
+  }
 }
