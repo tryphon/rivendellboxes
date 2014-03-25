daaf91f7fc039ad6b9346fc1e03eb9c3d29c825c
Alban Peignier <alban@tryphon.eu>
2014-03-25 20:43:16 +0100

    Use rivendell-import 0.8 and dedicated mysql database. Refs #1032. Refs #1033

--- files/rivendell-import/config.rb
+++ files/rivendell-import/config.rb
@@ -1,12 +1,17 @@
+# coding: utf-8
 Rivendell::Import.config do |config|
   config.to_prepare do |file|
-    file.in("music") do
-      cart.group = "MUSIC"
-    end
-  
+    # Les fichiers sont ignorés par défault
+    # Files are ignored by default
+    task.cancel!
+
+    # file.in("music") do
+    #   cart.group = "MUSIC"
+    # end
+
     # To delete file when task is completed
     #task.delete_file!
- 
-    cart.group ||= "TEMP"
+
+    # cart.group ||= "TEMP"
   end
 end
