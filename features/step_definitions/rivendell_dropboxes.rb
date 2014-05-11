a93fdd15d400e4caae5a945a7c04a9c310aa9ab7
Alban Peignier <alban@tryphon.eu>
2014-05-11 11:24:01 +0200

    Rewrite create_sound_file in rivendell_dropboxes steps to avoid tempfile unlink

--- features/step_definitions/rivendell_dropboxes.rb
+++ features/step_definitions/rivendell_dropboxes.rb
@@ -1,20 +1,19 @@
 require 'pathname'
 require 'taglib'
 
-def create_sound_file(attributes = {})
-  file = Tempfile.new(["sound_file", ".#{attributes[:format]}"])
-  file.close
+def create_sound_file(attributes = {}, &block)
+  Tempfile.open(["sound_file", ".#{attributes[:format]}"]) do |file|
+    system "sox -n -b 16 -r 48000 -c 2 -t #{attributes[:format]} #{file.path} synth 3:00 sine 1000 fade q 10 3:00 10" or raise "Can't create sound file"
 
-  system "sox -n -b 16 -r 48000 -c 2 -t #{attributes[:format]} #{file.path} synth 3:00 sine 1000 fade q 10 3:00 10" or raise "Can't create sound file"
+    TagLib::FileRef.open(file.path) do |fileref|
+      tag = fileref.tag
+      tag.title = attributes[:title] if attributes[:title]
 
-  TagLib::FileRef.open(file.path) do |fileref|
-    tag = fileref.tag
-    tag.title = attributes[:title] if attributes[:title]
+      fileref.save
+    end
 
-    fileref.save
+    yield file.path
   end
-
-  file.path
 end
 
 When /^a sound file is dropped into "([^"]*)" with title "([^"]*)"$/ do |dropbox_file, title|
@@ -25,21 +24,20 @@ When /^a sound file is dropped into "([^"]*)" with title "([^"]*)"$/ do |dropbox
     :format => 'mp3'
   }
 
-  file = create_sound_file attributes
+  create_sound_file(attributes) do |file|
+    current_box.ftp do |ftp|
+      ftp_file = Pathname.new("/dropboxes").join(dropbox_file)
 
-  current_box.ftp do |ftp|
-    ftp_file = Pathname.new("/dropboxes").join(dropbox_file)
-
-    ftp_file.dirname.descend do |directory|
-      begin
-        ftp.ls directory
-      rescue Net::FTPTempError
-        ftp.mkdir directory.to_s
+      # mkdir_p
+      ftp_file.dirname.descend do |directory|
+        begin
+          ftp.ls directory
+        rescue Net::FTPTempError
+          ftp.mkdir directory.to_s
+        end
       end
+
+      ftp.putbinaryfile file, ftp_file.to_s
     end
-
-    ftp.putbinaryfile file, ftp_file.to_s
   end
-
-  File.delete file
 end
