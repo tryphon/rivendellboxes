ab9b4daaecc681e86f4cf054cc0eb08054578da2
Alban Peignier <alban@tryphon.eu>
2014-05-11 23:46:05 +0200

    Try to fix dependency problem between policykit-1 and apt::source::pin on consolekit. Refs #1066

--- manifests/classes/consolekit.pp
+++ manifests/classes/consolekit.pp
@@ -1,10 +1,10 @@
 class consolekit {
-  package { [consolekit, policykit-1]: }
+  package { ['consolekit', 'policykit-1']: }
 
   if $debian::wheezy {
     apt::source::pin { [consolekit, libck-connector0]:
       source => "wheezy-backports",
-      before => Package[consolekit]
+      before => [Package['consolekit'], Package['policykit-1']]
     }
   }
 
