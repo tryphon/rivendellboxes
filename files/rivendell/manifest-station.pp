2a8314150fd43c46431ecc93041ff576fa2c71d5
Alban Peignier <alban@tryphon.eu>
2014-06-01 19:32:56 +0200

    Define a rivendell::station class to manage dependency with alsa class. Define default rivendell::rivendell_config when RME HDSP is detected. Refs #1087

--- files/rivendell/manifest-station.pp
+++ files/rivendell/manifest-station.pp
@@ -1,12 +1,22 @@
-dnsmasq::conf { rivendell_nas:
-  content => "address=/rivendellnas/${rivendell_nas}",
-  ensure => $rivendell_nas ? {
-    "" => absent,
-    default => present
+class rivendell::station {
+  include alsa
+  if $rivendell_config == "" and $alsa::device_id == "DSP" {
+    notice("Define default alsa_config for RME HDSP support");
+    $rivendell_config = { "alsa" => { "period_quantity" => "2" } }
+  }
+
+  dnsmasq::conf { rivendell_nas:
+   content => "address=/rivendellnas/${rivendell_nas}",
+   ensure => $rivendell_nas ? {
+     "" => absent,
+     default => present
+   }
+ }
+
+  file { "/var/etc/rd.conf":
+    content => template("/etc/puppet/templates/rivendell/rd.conf"),
+    tag => boot
   }
 }
 
-file { "/var/etc/rd.conf":
-  content => template("/etc/puppet/templates/rivendell/rd.conf"),
-  tag => boot
-}
+include rivendell::station
