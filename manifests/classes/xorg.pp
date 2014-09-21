38067b477b4effc074548ae2e87c0646ce3d4dcd
Alban Peignier <alban@tryphon.eu>
2014-09-21 17:38:33 +0200

    Blacklist gma500_gfx module. Refs #1166

--- manifests/classes/xorg.pp
+++ manifests/classes/xorg.pp
@@ -1,4 +1,9 @@
 class xorg {
   # make live easier for driver gma500_gfx
   package { 'xserver-xorg-video-modesetting': }
+
+  file { "/etc/modprobe.d/gma500_gfx.conf":
+    content => "blacklist gma500_gfx\n"
+  }
+  
 }
