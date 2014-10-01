23fe4355ba23d229dc4cd07af4639490dbfbfded
Alban Peignier <alban@tryphon.eu>
2014-10-01 23:09:15 +0200

    Update default rivendell-import config.rb. Refs #1196

--- files/rivendell-import/config.rb
+++ files/rivendell-import/config.rb
@@ -1,17 +1,25 @@
 # coding: utf-8
+
 Rivendell::Import.config do |config|
   config.to_prepare do |file|
-    # Les fichiers sont ignorés par défault
     # Files are ignored by default
+    # Les fichiers sont ignorés par défault
     task.cancel!
+
+    cart.default_title = file.basename
 
     # file.in("music") do
     #   cart.group = "MUSIC"
+    #   task.priority = 1
     # end
 
     # To delete file when task is completed
     #task.delete_file!
 
     # cart.group ||= "TEMP"
+    # task.priority ||= 10
   end
+
+  # Enable direct database access
+  config.db_url = 'mysql://rduser:letmein@localhost/Rivendell?reconnect=true'
 end
