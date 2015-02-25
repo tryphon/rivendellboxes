f9797d5cb7b4a2af8fe308e15882d827709310e6
Alban Peignier <alban@tryphon.eu>
2015-02-25 14:12:23 +0100

    Upgrade Rivendell Import to 1.02. Fixes #1328

--- manifests/classes/rivendell.pp
+++ manifests/classes/rivendell.pp
@@ -279,7 +279,7 @@ class rivendell::server {
 class rivendell::import {
   ruby::gem { 'rivendell-import':
     require => [Package['libsqlite3-dev'], Ruby::Gem['mysql']],
-    ensure => '1.01'
+    ensure => '1.02'
   }
   ruby::gem { mysql:
     require => Package[libmysqlclient-dev]
